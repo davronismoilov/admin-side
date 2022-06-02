0d3c9a12fa2249736984fa22eb7627fe1839a054
User <bekhzod.asadullayev@gmail.com>
2022-06-02 09:22:55 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'antd/dist/antd.css'
 import {BrowserRouter} from "react-router-dom";
 import Auth from "./admin/auth/auth";
+import MainLayout from "./admin/layout/main";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <React.StrictMode>
-            <Auth/>
+            {/*<Auth/>*/}
+            <MainLayout/>
         </React.StrictMode>
     </BrowserRouter>
 );
