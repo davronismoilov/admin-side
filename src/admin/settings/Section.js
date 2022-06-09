e7a02d22829944357f52bf51b463d129239613b2
User <bekhzod.asadullayev@gmail.com>
2022-06-09 12:33:35 +0500

    sad

--- src/admin/settings/Section.js
+++ src/admin/settings/Section.js
@@ -8,15 +8,16 @@ const {TabPane} = Tabs;
 const URL_FOR_GET_SECTION = "http://localhost:8080/api/v1/section/get";
 
 const Section = () => {
-    const [sectionList, setSectionList] = useState([]);
-    // let list = [];
-    const [data, setData] = useState({});
+    let [sectionList, setSectionList] = useState([]);
 
+    let [data, setData] = useState({});
+    let d;
     const handleData = (data) => {
-        setData(data);
+        setData({...data});
     }
 
-    useEffect(function () {
+
+     useEffect(function () {
         axios.get(URL_FOR_GET_SECTION, {headers: {Authorization: localStorage.getItem("accessToken")}}).then(res => {
             if (res.data.statusCode === 200) {
                 setSectionList(res.data.data);
@@ -26,9 +27,10 @@ const Section = () => {
                 }).then(res => {
                     console.log("before success", res.data.data);
                     if (res.data.statusCode === 200) {
-                        console.log("before set", res.data.data);
-                        handleData(res.data.data);
-                        console.log("after set", data);
+                        console.log("before set data", data);
+                        setData({...res.data.data})
+                        d= {...res.data.data}
+                        console.log("after set data", data);
                     }
                 })
             }
@@ -39,22 +41,26 @@ const Section = () => {
         //get single section by id
         axios.get(`${URL_FOR_GET_SECTION}/${id}`, {headers: {Authorization: localStorage.getItem("accessToken")}}).then(res => {
             if (res.data.statusCode === 200) {
-                setData(res.data.data);
+                // handleData(res.data.data)
+                d = {...res.data.data}
             }
+
         });
     }
 
     return (
         <Tabs type="card">
-            {sectionList.map((section) => {
-                console.log("section", section);
-                console.log("data", data);
-                return <TabPane tab={section.name} key={section.id} onClick={() => onChange(section.id)}>
-                    <Permissions data={data}/>
-                </TabPane>
-            })
-            }
+
+            {/*{*/}
+            {/*    d.content.map((section) => {*/}
+            {/*        console.log("section", section);*/}
+            {/*        console.log("data", d);*/}
+            {/*        return <TabPane tab={section.name} key={section.id} onClick={() => onChange(section.id)}>*/}
+            {/*            <Permissions data={data}/>*/}
+            {/*        </TabPane>*/}
+            {/*    })*/}
+            {/*}*/}
         </Tabs>
-    );
+    )
 }
 export default Section;
